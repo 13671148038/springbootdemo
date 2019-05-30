6e9b74d275a43504f4cbfff273570d19555048f3
13671148038 <8672252zhu>
2019-05-30 17:18:05 +0800

    添加页面

--- src/main/java/com/example/demo/config/MvcConfig.java
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -1,10 +1,6 @@
 package com.example.demo.config;
 
-import com.example.demo.interceptors.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ui.Model;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,16 +12,16 @@ public class MvcConfig implements WebMvcConfigurer {
 
     /**
      * 登录拦截器
+     *
      * @param registry
      */
     /*@Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor());
     }*/
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/task/toTaskManage").setViewName("task/task_manager");
         registry.addViewController("/user/toUserManage").setViewName("user/user_manager");

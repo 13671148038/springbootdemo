7eafb9ebe514f9c55068889745db334a6c890738
zhushaopeng <zhushaopeng@ybm100.com>
2020-10-20 20:02:20 +0800

    '添加自动配demo'

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.autoconfigure.MyTestAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class WebController {
 
+	@Autowired
+	MyTestAutoConfiguration myTestAutoConfiguration;
+
 	@GetMapping("/greeting")
 	public String toIndex(Model model, HttpServletRequest request){
         String remoteUser = request.getContextPath();
         model.addAttribute("name", remoteUser);
+		return "greeting";
+	}
+
+	@GetMapping("/getAutoConfiguration")
+	public String getAutoConfiguration(){
+
 		return "greeting";
 	}
 }

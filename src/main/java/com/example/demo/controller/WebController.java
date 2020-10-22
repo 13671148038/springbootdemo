fd745a2f55e94714dc3ff5e4d3b33dcad03b0c31
zhushaopeng <zhushaopeng@ybm100.com>
2020-10-22 19:46:12 +0800

    ''

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -1,15 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.autoconfigure.MyTestAutoConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-
-@Controller
+@Slf4j
+@RestController
 public class WebController {
 
 	@Autowired
@@ -24,7 +26,7 @@ public class WebController {
 
 	@GetMapping("/getAutoConfiguration")
 	public String getAutoConfiguration(){
-
+		log.info("====================================================================");
 		return "greeting";
 	}
 }
--- src/main/java/com/example/demo/interceptors/LoginInterceptor.java
+++ src/main/java/com/example/demo/interceptors/LoginInterceptor.java
@@ -1,14 +1,19 @@
 package com.example.demo.interceptors;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
+@Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        MDC.put("trace_uuid", UUID.randomUUID().toString());
 //        String servletPath = request.getServletPath();
 //        servletPath = servletPath.toLowerCase();
 //        System.out.println(servletPath);
@@ -30,11 +35,12 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        log.info("postHandle==============");
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
+        log.error("ex:", ex);
+        log.info("afterCompletion==============");
     }
 
     /* public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,

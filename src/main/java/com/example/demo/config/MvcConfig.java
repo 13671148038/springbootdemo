71f6d693d96a0a2f85011054325c11affc76ec8d
zhushaopeng <13671148038@163.com>
2021-01-14 20:59:54 +0800

    xx

--- src/main/java/com/example/demo/config/MvcConfig.java
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -1,11 +1,15 @@
 package com.example.demo.config;
 
 import com.example.demo.interceptors.LoginInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * Created by MyPC on 2018/7/17.
@@ -19,7 +23,9 @@ public class MvcConfig implements WebMvcConfigurer {
      * @param registry
      */
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor());
+        registry.addInterceptor(new LoginInterceptor()).
+                //swagger不拦截
+                        excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
     }
 
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -35,4 +41,44 @@ public class MvcConfig implements WebMvcConfigurer {
 //        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/");
 //        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
     }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        /**
+         * 序列换成json时,将所有的long变成string
+         * 因为js中得数字类型不能包含所有的java long值
+         * 或者在实体类里面long类型上面加上
+         * @JsonSerialize(using = ToStringSerializer.class)
+         * 两种类型都可以
+         */
+//        SimpleModule simpleModule = new SimpleModule();
+//        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+//        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+//        objectMapper.registerModule(simpleModule);
+
+        //返回的时间格式转换或者在返回实体里面的时间上面加上注解@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        //两种类型都可以实现返回时间格式化
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(0, jackson2HttpMessageConverter);
+    }
+
+    /**
+     * 解决跨域问题
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowedOrigins("*"). //允许跨域的域名，可以用*表示允许任何域名使用
+                allowedMethods("*"). //允许任何方法（post、get等）
+                allowedHeaders("*"). //允许任何请求头
+                allowCredentials(true). //带上cookie信息
+                exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(3600L); //maxAge(3600)表明在3600秒内，不需要再发送预检验请求，可以缓存该结果
+    }
+
 }
